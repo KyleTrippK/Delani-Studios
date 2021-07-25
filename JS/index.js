bea6f9dbb0841b02e296aa4aa97d1cbdf7715104
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-07-25 13:43:39 +0300

    will validate the input forms and returns an alert for leaving the froms empty

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
     $("section .header").addClass('text-center text-white');
-    $("#what-we-do").addClass('justify-content-center ml-5');
+    // $("#what-we-do").addClass('justify-content-center ml-5');
     $("#design").click(function(){$("#design-hide").toggle(500)});
     $("#development").click(function(){$("#development-hide").toggle(500)});
     $("#product").click(function(){$("#product-hide").toggle(500)});
-    $("#icons").addClass("text-center");
+    $("#contact-us").addClass("text-center");
+    $("#icons").addClass("text-center mt-3");
+    $("#contains").addClass("text-center");
 })
+
+// Form verification
+function verification(){
+    var nameInput = document.getElementById("name-input").value;
+    var mailInput = document.getElementById("mail-input").value;
+    if(nameInput==""){
+        alert("Please fill out the Name input");
+        return false
+    }
+    if(mailInput==""){
+        alert("Please fill out your email")
+        return false
+    }
+}
