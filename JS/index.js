cd3cce19623f9034c434512e41584c5e55f10ee6
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-07-25 19:22:09 +0300

    adds an hover effect on the portfolios

--- JS/index.js
+++ JS/index.js
@@ -2,12 +2,21 @@ $(document).ready(function(){
     $("section .header").addClass('text-center text-white');
     // $("#what-we-do").addClass('justify-content-center ml-5');
     $("#design").click(function(){$("#design-hide").toggle(500)});
+    $("#design").click(function(){$("#hidden").toggle(100)});
     $("#development").click(function(){$("#development-hide").toggle(500)});
     $("#product").click(function(){$("#product-hide").toggle(500)});
     $("#contact-us").addClass("text-center");
     $("#icons").addClass("text-center mt-3");
     $("#contains").addClass("text-center");
-    $(".submit").addClass("text-center");
+    $(".submit-div").addClass("text-center");
+    //hover on portfolio 1
+    $("#bailey1, baileyword1").hover(function(){
+        $("#baileyword1").css("display", "block").fadeIn();
+    },
+    function(){
+        $("#baileyword1").fadeOut();
+    })
+    $("#bailey1")
 })
 
 // Form verification
@@ -24,3 +33,7 @@ function verification(){
     }
     alert("Welcome "+ nameInput);
 }
+
+
+
+    
